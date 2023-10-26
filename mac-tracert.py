aa1b3c1f47d72fbf86d2076ab7057b0ac01bf3df
MAKE <2975609919@qq.com>
2023-10-26 23:02:59 +0800

    修改

--- mac-tracert.py
+++ mac-tracert.py
@@ -1,17 +1,30 @@
 from netaddr import *
 
+import re
+
+#输入要转换的MAC
+def mac_address_input():
+    in_mac_sour = input('输入要转换的MAC：')
+    return in_mac_sour
+
 
 # mac地址转换 EUI to 华为
 def mac_address_format_conversion__eui_to_huawei(souer_mac):
-    mac = EUI(in_mac_sour)
-
-    mac.dialect = mac_cisco
+    try:
+        # 如果错误则退出程序
+        mac = EUI(in_mac_sour)
+    except:
+        print('输入的MAC地址有误！')
+        exit()
 
     mac = str(mac).replace('.', '-')  # 直接用字符串处理！
 
     return mac
 
 
-in_mac_sour = input('输入要转换的MAC：')
-out_mac = mac_address_format_conversion__eui_to_huawei(in_mac_sour)
-print(out_mac)
+if __name__ == '__main__':
+    # 输出后不要退出，输入任意键后退出
+    in_mac_sour = mac_address_input()
+    out_mac = mac_address_format_conversion__eui_to_huawei(in_mac_sour)
+    print(out_mac)
+    input('按任意键退出...')
